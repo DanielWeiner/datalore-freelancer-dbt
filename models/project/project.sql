3483261e79a61012a6e14c8934311e24a072f4e4
Daniel Weiner <danielbweiner@gmail.com>
2025-03-08 14:35:01 -0600

    eliminate duplicate rows using ROW_NUMBER

--- models/project/project.sql
+++ models/project/project.sql
@@ -1,1 +1,7 @@
-select * from {{ source('freelancer_projects', 'projects') }}
+WITH source_table_numbered AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY `id`, `execution_time`) AS row_number 
+    FROM {{ source('freelancer_projects', 'projects') }}
+)
+SELECT * EXCEPT(row_number)
+FROM source_table_numbered
+WHERE row_number = 1
